fd01f8da4657b8771f73f47722231fc52a6cbb76
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-01-15 17:12:50 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [operator,setOperator] = useState("");
+  const [value,setValue] = useState("");
+  const [total,setTotal] = useState("");
+  
+  function click(event){
+    const btnValue = event.target.value;
+    setValue(value + btnValue);
+  }
+  function clear(){
+    setValue("");
+    setTotal("");
+  }
+  function action(event){
+    const x = event.target.value;
+    setOperator(operator+x);
+    setTotal(total + value);
+    setValue("");
+  }
+  function equal(){
+    if(operator === "+"){
+      setValue(Number(total)+Number(value));
+    }else if(operator === "-"){
+      setValue(Number(total)-Number(value));
+    }else if(operator === "/"){
+      setValue(Number(total)/Number(value));
+    }else{
+      setValue(Number(total)*Number(value));
+    }
+    setOperator("");
+    setTotal(total+value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="grid-container">
+          <h1 className='display'>{value}</h1>
+          <input type='submit' value="Clear" className='item1 item' onClick={clear} />
+          <input type='submit' value="/" className='item' onClick={action} />
+          <input type='submit' value="*" className='item' onClick={action} />
+          <input type='submit' value="7" className='item' onClick={click} />
+          <input type='submit' value="8" className='item' onClick={click} />
+          <input type='submit' value="9" className='item' onClick={click} />
+          <input type='submit' value="-" className='item' onClick={action} />
+          <input type='submit' value="4" className='item' onClick={click} />
+          <input type='submit' value="5" className='item' onClick={click} />
+          <input type='submit' value="6" className='item' onClick={click} />
+          <input type='submit' value="+" className='item2 item' onClick={action} />
+          <input type='submit' value="1" className='item' onClick={click} />
+          <input type='submit' value="3" className='item' onClick={click} />
+          <input type='submit' value="2" className='item' onClick={click} />
+          <input type='submit' value="0" className='item' onClick={click} />
+          <input type='submit' value="00" className='item' onClick={click} />
+          <input type='submit' value="." className='item' onClick={click} />
+          <input type='submit' value="=" className='item3 item' onClick={equal} />
+        </div>
     </div>
   );
 }
